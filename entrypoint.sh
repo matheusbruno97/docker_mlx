ccae812f4989885f49eca274c7463f4c9c02619f
matheusbruno97 <128164927+matheusbruno97@users.noreply.github.com>
2024-05-08 21:23:57 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,8 @@
 # Debugging purposes
 # whoami && arch && uname && cat /etc/*-release && df -h && free -h &&
 
-export DISPLAY=:99
-
-Xvfb $DISPLAY -screen 0 2048x1200x16 -ac &
-
-pip install requests selenium
-
 # Start Xvfb and Multilogin X agent
-/opt/mlx/agent.bin &
+xvfb-run /opt/mlx/agent.bin &
 
 # Wait for a while so the launcher is properly installed
 sleep 20
