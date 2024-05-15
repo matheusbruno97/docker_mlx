9bad03a07caf769fa3d4abbcd4f0f237ff62e581
matheusbruno97 <128164927+matheusbruno97@users.noreply.github.com>
2024-05-15 00:24:44 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Debugging purposes
-# whoami && arch && uname && cat /etc/*-release && df -h && free -h &&
+pip install --user requests selenium
 
-# Start Xvfb and Multilogin X agent
-xvfb-run /opt/mlx/agent.bin &
-
-# Wait for a while so the launcher is properly installed
-sleep 20
+curl --location --fail --output mlxdeb.deb "https://mlxdists.s3.eu-west-3.amazonaws.com/mlx/1.15.0/multiloginx-amd64.deb" && \
+sudo dpkg -i mlxdeb.deb
 
 # Run Python automation script
-cd /app/mlx-app && python3 main.py
+cd /home/mlx-user/app/ &&
+
+# Start Xvfb and Multilogin X agent
+xvfb-run /opt/mlx/agent.bin & sleep 15 && python3 main.py
